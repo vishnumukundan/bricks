77c8390c1ea849ca1a7c910f43a06b06dd61ba65
vishnu <vishnumukundan040@gmail.com>
2024-08-03 21:59:13 +0530

    fix: added required package install commands into pre_gen

--- feature/hooks/pre_gen.dart
+++ feature/hooks/pre_gen.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {}
+void run(HookContext context) async {
+  // Run `flutter packages get` after generation.
+  final progress = context.logger.progress('Installing packages');
+  await Process.run('flutter', ['pub', 'add', 'flutter_bloc']);
+  await Process.run('flutter', ['pub', 'add', 'flutter_hooks']);
+  await Process.run('flutter', ['pub', 'add', 'json_annotation']);
+  await Process.run('flutter', ['pub', 'add', 'equatable']);
+  await Process.run('flutter', ['pub', 'add', 'freezed_annotation']);
+  await Process.run('flutter', ['pub', 'add', 'dev:build_runner']);
+  await Process.run('flutter', ['pub', 'add', 'dev:json_serializable']);
+  await Process.run('flutter', ['pub', 'add', 'dev:freezed']);
+  progress.complete();
+}
